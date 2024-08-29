d96d6cdb2fa57f86317c282b5b1ab1dd6e244e44
vi58-coder <151148480+vi58-coder@users.noreply.github.com>
2024-08-29 22:08:13 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const imageconEl = document.querySelector(".image-container");
+
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("click", ()=>{
+   imageNum = 10
+   addNewImages()
+});
+
+
+function addNewImages(){
+    for (let index = 0; index < imageNum; index++) {
+        const newImageEl = document.createElement("img"); 
+    newImageEl.src= `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`;
+
+    imageconEl.appendChild(newImageEl);
+        
+    }
+    
+}
